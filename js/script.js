41d10670f1cb31913efb4ceb8cbbb319b85df108
riteshnath <nathritesh98@gmail.com>
2023-09-10 12:01:37 +0530

    Sticky Nav

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+/////////////////////////STICKY NAVIGATION////////////
+// Not displaying the sticky navigation in the hero section but only after the next section.
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.querySelector(".header").classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.querySelector(".header").classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+  }
+);
+obs.observe(sectionHeroEl);
